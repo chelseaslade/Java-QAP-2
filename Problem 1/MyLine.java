dee7158bf95aa612bcd2cc37f15d8d293c29e159
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-10-01 10:33:14 -0230

    Line Test start

--- Problem 1/MyLine.java
+++ Problem 1/MyLine.java
@@ -1,4 +1,25 @@
-
 public class MyLine {
-    
+    //Attributes
+    public MyPoint beginPoint;
+    public MyPoint endPoint;
+
+    //Default Constructor
+    public MyLine(MyPoint beginPoint, MyPoint endPoint)
+    {
+        this.beginPoint = beginPoint;
+        this.endPoint = endPoint;
+    }
+
+    //Parameterized Constructor
+    public MyLine(int x1, int y1, int x2, int y2)
+    {
+        beginPoint = new MyPoint(x1, y1);
+        endPoint = new MyPoint(x2, y2);
+    }
+
+    //Methods
+    public void getBegin()
+    {
+        System.out.println(beginPoint);
+    }
 }
--- Problem 1/TestMyLine.java
+++ Problem 1/TestMyLine.java
@@ -1,4 +1,13 @@
 
 public class TestMyLine {
-    
+    public static void main(String[] args)
+    {
+        MyPoint beginPoint = new MyPoint(1,3);
+        MyPoint endPoint = new MyPoint(2,4);
+
+        MyLine lineOne = new MyLine(beginPoint, endPoint);
+        MyLine lineTwo = new MyLine(1, 2, 2, 4);
+
+        
+    }
 }
