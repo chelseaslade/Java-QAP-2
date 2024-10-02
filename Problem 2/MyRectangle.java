2b6c56772781a8a8eba84b1c5f16b0b9bddcf17f
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-10-02 12:38:24 -0230

    Tested set/get functions

--- Problem 2/MyRectangle.java
+++ Problem 2/MyRectangle.java
@@ -19,12 +19,28 @@ public class MyRectangle {
 
     //Methods
     //getTopLeft()
+    public MyPoint getTopLeft()
+    {
+        return this.topLeftPoint;
+    }
 
     //setTopLeft()
+    public void setTopLeft(int x1, int y1)
+    {
+        this.topLeftPoint = new MyPoint(x1, y1);
+    }
 
     //getBottomRight()
+    public MyPoint getBottomRight()
+    {
+        return this.bottomRightPoint;
+    }
 
     //setBottomRight()
+    public void setBottomRight(int x2, int y2)
+    {
+        this.bottomRightPoint = new MyPoint(x2, y2);
+    }
 
     //toString()
 
--- Problem 2/TestMyRectangle.java
+++ Problem 2/TestMyRectangle.java
@@ -3,7 +3,26 @@ public class TestMyRectangle {
     public static void main(String[] args)
     {
         //Set objects
+        MyPoint topLeftPoint = new MyPoint(1,2);
+        MyPoint bottomRightPoint = new MyPoint(4,5);
+
+        MyRectangle rectangleOne = new MyRectangle(topLeftPoint, bottomRightPoint);
 
         //Test Methods
+        //getTopLeft()
+        System.out.println(rectangleOne.getTopLeft());
+
+        //setTopLeft()
+        rectangleOne.setTopLeft(2,3);
+        System.out.println(rectangleOne.topLeftPoint);
+
+        //getBottomRight()
+        System.out.println(rectangleOne.getBottomRight());
+
+        //setBottomRight()
+        rectangleOne.setBottomRight(5, 6);
+        System.out.println(rectangleOne.bottomRightPoint);
+
+        //toString()
     }
 }
