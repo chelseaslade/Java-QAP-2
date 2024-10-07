9dfaafddd3ba233f7408af355a045c8df324929e
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-10-07 16:51:06 -0230

    Added println titles for method readability

--- Problem 2/TestMyRectangle.java
+++ Problem 2/TestMyRectangle.java
@@ -10,29 +10,37 @@ public class TestMyRectangle {
 
         //Test Methods
         //getTopLeft()
+        System.out.println("Get Top Left: ");
         System.out.println(rectangleOne.getTopLeft());
 
         //setTopLeft()
+        System.out.println("Set Top Left: ");
         rectangleOne.setTopLeft(2,3);
         System.out.println(rectangleOne.getTopLeft());
 
         //getBottomRight()
+        System.out.println("Get Bottom Right: ");
         System.out.println(rectangleOne.getBottomRight());
 
         //setBottomRight()
+        System.out.println("Set Bottom Right: ");
         rectangleOne.setBottomRight(5, 1);
         System.out.println(rectangleOne.getBottomRight());
 
         //getHeight()
+        System.out.println("Get Height: ");
         System.out.println(rectangleOne.getHeight());
 
         //getWidth()
+        System.out.println("Get Width: ");
         System.out.println(rectangleOne.getWidth());
 
         //getArea()
+        System.out.println("Get Area: ");
         System.out.println(rectangleOne.getArea());
 
         //toString()
+        System.out.println("toString: ");
         System.out.println(rectangleOne.toString());
     }
 }
